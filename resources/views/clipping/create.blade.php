abbec32c66acb0f7b1c36a9eb84764d749b8f6a2
alinetenorio <aline.tenorio96@gmail.com>
2020-08-04 21:46:11 -0300

    adicionando campos de inserção manual

--- resources/views/clipping/create.blade.php
+++ resources/views/clipping/create.blade.php
@@ -11,14 +11,28 @@
 
                 <form method="post" action="{{ route('clipping.gerar') }}">
                     @csrf
+                    <div>
+                      <label>Data Inicial</label>
+                      <input name="dataInicio" placeholder="dd/mm/aaaa">
 
-                    <label>Data Inicial</label>
-                    <input name="dataInicio" placeholder="dd/mm/aaaa">
+                      <label>Data Final</label>
+                      <input name="dataFinal" placeholder="dd/mm/aaaa">
+                    </div>
+                    
+                    <br>
 
-                    <label>Data Final</label>
-                    <input name="dataFinal" placeholder="dd/mm/aaaa">
+                    <div id="paginasNovas">
+                      <p>Inserir em <b>"Páginas novas, atualizadas ou destaques"</b></p>
+                      <label>Título da postagem</label>
+                      <input name="titulo[]">
+                      <label>Link</label>
+                      <input name="link[]">
 
-                    <button type="submit" class="btn btn-primary">Gerar</button>
+                    </div>
+                      <a href="#" onclick="addCampo()" class="btn btn-primary" id="addCoautor">Adicionar participante</a>
+                      <a href='#' onclick="removerCampo()" class="btn btn-danger mt-2 mb-2 delete">Remover participante</a>
+
+                      <button type="submit" class="btn btn-primary">Gerar</button>
                 </form>
               </div>
             </div>
@@ -26,3 +40,35 @@
         </div> 
 </div>
 @endsection
+
+@section('javascript')
+<script type="text/javascript">
+  
+  function addCampo(){
+    paginasNovas = document.getElementById('paginasNovas');
+    
+    titulo = document.createElement("LABEL");
+    titulo.innerHTML = "Título da postagem";
+    tituloInput = document.createElement("INPUT");
+    tituloInput.setAttribute('name', 'titulo[]');
+    link = document.createElement("LABEL");
+    link.innerHTML = "Link";
+    linkInput = document.createElement("INPUT")
+    linkInput.setAttribute('name', 'link[]');;
+
+    paginasNovas.appendChild(titulo);
+    paginasNovas.appendChild(tituloInput);
+    paginasNovas.appendChild(link);
+    paginasNovas.appendChild(linkInput);      
+  }
+
+  function removerCampo(){
+    paginasNovas = document.getElementById('paginasNovas');
+    
+    for(i = 0; i < 4; i++){
+      length = paginasNovas.childNodes.length;
+      paginasNovas.removeChild(paginasNovas.childNodes[length-1]);         
+    }
+  }
+
+</script>
